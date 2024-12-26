eeebb069e72624293c9bd98e65a257fb6c38ff59
maayanshani <maayan.shani@mail.huji.ac.il>
2024-12-26 12:53:10 +0200

    Constants class

---
+++ pepse/util/Constants.java
@@ -0,0 +1,35 @@
+package pepse.util;
+
+/**
+ * A utility class to store commonly used constants throughout the project.
+ *
+ * <h2>How to Use</h2>
+ * <p>You can access the constants using the class name directly, as they are
+ * declared as <code>public static final</code>. Example usage:</p>
+ *
+ * <pre>
+ * // Access a string constant
+ * String example = Constants.EXAMPLE;
+ *
+ * // Use a numeric constant
+ * int doubledValue = Constants.NUM_EXAMPLE * 2;
+ *
+ * // Use a boolean constant
+ * if (Constants.DEBUG_MODE) {
+ *     System.out.println("Debug mode is enabled.");
+ * }
+ * </pre>
+ */
+public class Constants {
+
+    /** Numeric constants */
+    public static final String EXAMPLE = "example";
+
+    /** String constants */
+    public static final int num_EXAMPLE = 1;
+
+
+    /** Boolean constants */
+
+
+}
